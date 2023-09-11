032534019e422423291d74778f6846e159095df2
Ethan S <es5888@nyu.edu>
2023-09-10 20:36:45 -0400

    Figure out why no particles are showing up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
-import React from 'react';
 import About from './components/About';
 import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-
 
 function App() {
+  useEffect(() => {
+    if (window.particlesJS) {
+      window.particlesJS.load('particles-js', '/particles-config.json', () => {
+        console.log('particles.js loaded - callback');
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
+      <div id="particles-js" style={{ position: 'absolute', width: '100%', height: '100%', zIndex: -1 }}></div>
       <About />
       <Experience />
-      <Projects />
-      <Skills />
-      {/* ... other components as you create them */}
     </div>
   );
 }
