523edd3e6f0d70e976200a2b8a6f2054a9249353
Ethan S <es5888@nyu.edu>
2023-09-14 17:55:45 -0400

    Added experiences and profile picture, continue to add content and make the site responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import closeHeadshot from '../images/close_headshot.png';
 
 function About() {
   return (
     <section id="about" className="bg-gray-800 p-8 rounded-md shadow-lg my-6">
-      <h2 className="text-2xl font-bold mb-4 text-gray-300">About Me</h2>
-      <p className="text-gray-400 leading-relaxed">
-        Hi, I'm Ethan. I've built a career around cybersecurity, ensuring data stays where it belongs. I value the intricacies of the digital landscape and the importance of mental well-being in our connected world.
-      </p>
-      <p className="text-gray-400 leading-relaxed mt-4">
-        Beyond my professional realm, I have a keen interest in finance and the endless possibilities of innovation. I believe in consistent growth, both personally and in the ever-evolving world of technology.
-      </p>
-      {/* Consider adding a profile picture or any other relevant content */}
+      <div className="flex items-center">
+        <img 
+            src={closeHeadshot} 
+            alt="Ethan Sam" 
+            style={{ width: '10vw', height: '10vw', maxWidth: '150px', maxHeight: '150px' }}
+            className="mr-6 rounded-full shadow-lg" 
+        />
+        <div>
+          <h2 className="text-2xl font-bold mb-4 text-gray-300">About Me</h2>
+          <p className="text-gray-400 leading-relaxed">
+            Hi, I'm Ethan. I've built a career around cybersecurity, ensuring data stays where it belongs. I value the intricacies of the digital landscape and the importance of mental well-being in our connected world.
+          </p>
+          <p className="text-gray-400 leading-relaxed mt-4">
+            Beyond my professional realm, I have a keen interest in finance and the endless possibilities of innovation. I believe in consistent growth, both personally and in the ever-evolving world of technology.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
