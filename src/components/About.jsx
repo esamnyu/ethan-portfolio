ba8df0942b45ad07c107a49ddf1025e8e736cd28
Ethan S <es5888@nyu.edu>
2023-09-20 18:11:28 -0400

    Updated layout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,19 +3,21 @@ import closeHeadshot from '../images/close_headshot.png';
 
 function About() {
   return (
-    <section id="about" className="my-6">
-      <div className="bg-gray-800 p-4 md:p-8 rounded-md shadow-lg grid md:grid-cols-2 gap-2 items-center">
+    <section id="about" className="bg-gray-800 p-4 md:p-8 rounded-md shadow-lg my-6 max-w-5xl mx-auto">
+      <h2 className="text-xl md:text-2xl font-bold mb-4 text-gray-300">About Me</h2>
+
+      <div className="flex flex-col md:flex-row gap-2 items-center md:items-start">
         
         <img 
             src={closeHeadshot} 
             alt="Ethan Sam" 
-            className="w-72 md:w-102 lg:w-129 xl:w-173 rounded-full shadow-md m-auto" 
+            className="w-72 md:w-96 lg:w-112 xl:w-128 rounded-full shadow-md m-auto" 
         />
         
-        <div className="bg-gray-700 p-2 md:p-4 rounded-md space-y-4">
-          <h2 className="text-xl md:text-2xl font-bold text-gray-300">About Me</h2>
+        <div className="bg-gray-700 p-2 md:p-4 rounded-lg space-y-4 flex-1">
           <p className="text-gray-400 leading-relaxed">
-          Hello everyone, I'm Ethan! I've spent a significant part of my career as a Cybersecurity Auditor, dedicating 2 years to ensuring digital safety across city agencies. My foundation in this domain is reinforced by a Computer Science degree, providing me with both theoretical and practical insights. As I look ahead, my ambition is to pivot into an engineering-centric role within cybersecurity, eager to tackle new challenges and further my expertise.          </p>
+            Hello everyone, I'm <span className="font-bold text-gray-300">Ethan</span>! I've spent a significant part of my career as a <span className="text-blue-400 font-bold">Cybersecurity Auditor</span>, dedicating 2 years to ensuring digital safety across city agencies. My foundation in this domain is reinforced by a <span className="text-blue-400 font-bold">Computer Science degree</span>, providing me with both theoretical and practical insights. As I look ahead, my ambition is to pivot into an <span className="italic">engineering-centric role</span> within cybersecurity, eager to tackle new challenges and further my expertise.
+          </p>
         </div>
         
       </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,48 +2,26 @@ import React from 'react';
 
 function Experience() {
     return (
-        <section id="experience" className="bg-gray-700 p-8 rounded-md shadow-lg my-6">
+        <section id="experience" className="bg-gray-800 p-8 rounded-md shadow-lg my-6">
             <h2 className="text-2xl font-bold mb-4 text-gray-300">Experience</h2>
             
-            <div className="mb-6">
-                <h3 className="text-xl font-medium text-gray-300">Office of Technology & Innovation (OTI): Cyber Command</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {/* ... Each experience card ... */}
+
+                <div className="bg-gray-700 p-6 rounded-lg shadow-md">
+                    <h3 className="text-xl font-medium text-blue-400 mb-2">Cybersecurity Auditor</h3>
+                    <p className="text-gray-400">Office of Technology & Innovation (OTI): Cyber Command | December 2021 – August 2023</p>
+                    <div className="mt-4 space-y-2">
+                        <p className="text-gray-400">- Administered ICS questionnaire for mission-critical OT/ICS systems.</p>
+                        <p className="text-gray-400">- Collaborated with agency executives.</p>
+                        <p className="text-gray-400">- Analyzed comptroller's office reports.</p>
+                    </div>
+                    <button className="mt-4 bg-blue-500 text-gray-100 py-2 px-4 rounded hover:bg-blue-600">Read More</button>
+                </div>
+
+                {/* ... Repeat for other experiences ... */}
                 
-                <p className="text-gray-400">
-                    <strong>Cybersecurity Auditor</strong> | December 2021 – August 2023 <br />
-                    - Administered ICS questionnaire for mission-critical OT/ICS systems, ensuring compliance with NIST frameworks. <br />
-                    - Collaborated with agency executives to identify cybersecurity vulnerabilities. <br />
-                    - Analyzed comptroller's office reports, refining audit processes.
-                </p>
-
-                <p className="text-gray-400 mt-2">
-                    <strong>Cybersecurity Analyst Intern</strong> | June 2021 – August 2021 <br />
-                    - Conducted evaluations of NYC agencies' cybersecurity programs. <br />
-                    - Developed an internal Audit Guide to streamline processes. <br />
-                    - Engaged in mock trials preparing for agency audit feedback.
-                </p>
             </div>
-
-            <div className="mb-6">
-                <h3 className="text-xl font-medium text-gray-300">Electronic Gaming Federation</h3>
-                
-                <p className="text-gray-400">
-                    <strong>Software Engineer Intern</strong> | January 2020 – June 2020 <br />
-                    - Oversaw the creation of a collegiate Elo ranking system. <br />
-                    - Collaborated in agile/scrum methodologies to implement a web app MVP.
-                </p>
-            </div>
-
-            <div className="mb-6">
-                <h3 className="text-xl font-medium text-gray-300">Codepath</h3>
-                
-                <p className="text-gray-400">
-                    <strong>Cybersecurity Fellow</strong> | Sept. 2018 – Sept 2019 <br />
-                    - Completed a selective Cybersecurity program. <br />
-                    - Led recruitment efforts for an educational program, empowering underrepresented individuals.
-                </p>
-            </div>
-
-            {/* ... Add other experiences similarly ... */}
         </section>
     );
 }
